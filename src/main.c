a5650335782dce14518930d0a439c1c9422a998e
Elana Zobin <leka@gnode.org>
2022-03-16 16:15:57 -0700

    progress

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]) {
+
+   if(argc == 1){
+      printf("No user message specified - error condition\n");
+      exit(1);
+   }
+
+   printf("Test Program has completed with user message: %s\n", argv[1]);
+   exit(0);
 }
